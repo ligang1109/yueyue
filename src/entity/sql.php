d5b952319e3dff4879c4d0e6be45fc610e636c9b
ligang <ligang1109@gmail.com>
2014-08-23 21:47:21 +0800

    stage

--- src/entity/sql.php
+++ src/entity/sql.php
@@ -3,7 +3,7 @@ namespace YueYue\Entity;
 
 interface Sql
 {/*{{{*/
-    public function bindSqlAttrs($gen_sql_attrs_value=true);
+    public function addSqlAttrs($gen_sql_attrs_value=true);
     public function getSqlUpdateFields($params=array());
     public function setAttrsBySqlItem($item=array());
 }/*}}}*/
